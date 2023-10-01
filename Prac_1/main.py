86879510e89f3d36997e708d620f32579349cdf1
Cés ( <90875740+Ceeeeeeees@users.noreply.github.com>
2023-10-01 00:20:06 -0600

    Renderizado
    clase game, proximamente player

--- Prac_1/main.py
+++ Prac_1/main.py
@@ -1,27 +1,58 @@
+import pygame
 from class_Map import Board
-from class_Agent import Agent
 from class_Agent import Monkey
-"""
-tablero = Board("Prac_1/board1.txt")
-tablero.show_board()
+from class_Game import Rend
 
-tablero.update_cel_values(1, 2, [5, 0, 0]) #pos row, pos col, values
-print("UPDATED")
-tablero.show_board()
+# Inicializar Pygame
+pygame.init()
 
-"""
+# Tamaño de la ventana y celda
+ANCHO_VENTANA = 800
+ALTO_VENTANA = 600
+TAMANO_CELDA = 30
 
+# Crear una instancia de la clase Board con el archivo deseado
+archivo_tablero = "Prac_1/board1.txt"
+tablero = Board(archivo_tablero, (1, 1), (3, 3))
 
-tablero = Board("Prac_1/board.txt", (1,1), (3,3))
-
+# Crear una instancia de la clase Agent con el tablero y el tipo Monkey
 agente = Monkey(tablero)
 
-tablero.show_board()
+# Crear una instancia de la clase Rend con el tablero y el agente
+rend = Rend(tablero, agente, TAMANO_CELDA)
 
-agente.mover_abajo()
-agente.mover_abajo()
-agente.mover_abajo()
-agente.mover_der()
-agente.mover_abajo()
+# Crear la ventana
+ventana = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
+pygame.display.set_caption("Dibujando Tablero")
 
-agente.show_current_cost()
+bola_imagen = pygame.image.load("bola.png")  # Carga la imagen de la bola
+nueva_ancho = 25  # Ancho de la celda
+nueva_alto = 25  # Alto de la celda
+bola_imagen = pygame.transform.scale(bola_imagen, (nueva_ancho, nueva_alto))
+
+
+# Bucle principal
+ejecutando = True
+while ejecutando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            ejecutando = False
+        elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                agente.mover_izq()
+            elif evento.key == pygame.K_RIGHT:
+                agente.mover_der()
+            elif evento.key == pygame.K_UP:
+                agente.mover_arriba()
+            elif evento.key == pygame.K_DOWN:
+                agente.mover_abajo()
+
+    ventana.fill((0, 0, 0))  # Llenar la ventana con color negro
+    rend.dibujar_mapa(ventana)
+
+    # Dibujar la bola en la posición actual del agente
+    ventana.blit(bola_imagen, (agente.pos_actual[0] * TAMANO_CELDA, agente.pos_actual[1] * TAMANO_CELDA))
+
+    pygame.display.update()  # Agregar esta línea para actualizar la ventana
+
+pygame.quit()

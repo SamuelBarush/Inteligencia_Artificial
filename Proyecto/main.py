762341f860bd1654b451764b67f8fb3584e4619d
SamuelBarush <soviedor1700@alumno.ipn.mx>
2023-10-26 10:53:21 -0600

    Actu

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,4 +1,5 @@
 from class_Map import Board
+from class_MapAgent import BoardAgent
 from class_Agent import *
 import pygame
 
@@ -8,7 +9,7 @@ ANCHO_VENTANA = 800
 ALTO_VENTANA = 600
 TAMANO_CELDA = 30
 
-# Create an instance of the Board class with the desired file
-archivo_tablero = "../Prac_1/board1.txt"
+archivo_tablero = "./board1.txt"
 tablero = Board(archivo_tablero, (1, 1), (14, 14), (7, 7), (7, 7), (7, 7))
 
+pygame.quit()

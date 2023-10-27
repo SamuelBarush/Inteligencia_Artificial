f83c07f4b906b0de2e94972041aab058f2a05ca2
SamuelBarush <soviedor1700@alumno.ipn.mx>
2023-10-27 15:09:33 -0600

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,5 +1,4 @@
 from class_Map import Board
-from class_MapAgent import BoardAgent
 from class_Agent import *
 import pygame
 
@@ -9,7 +8,5 @@ ANCHO_VENTANA = 800
 ALTO_VENTANA = 600
 TAMANO_CELDA = 30
 
-archivo_tablero = "./board1.txt"
-tablero = Board(archivo_tablero, (1, 1), (14, 14), (7, 7), (7, 7), (7, 7))
 
 pygame.quit()

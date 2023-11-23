54cbf8f5f7ed6e3ec4c7df8e996a443c10668858
Samuel Barush <100962136+SamuelBarush@users.noreply.github.com>
2023-11-22 19:40:23 -0600

    Final Version 1.0

--- PL1/class_data.py
+++ PL1/class_data.py
@@ -1,9 +1,10 @@
 class Data:
-    def __init__(self, file_path,delimiter) -> None:
+    def __init__(self, file_path, delimiter, attributes) -> None:
         # Define the path to the text file
-        self.file_path = file_path
-        self.data = self.read_data(self.file_path)
+        self.file_path = file_path        
         self.delimiter = delimiter
+        self.attributes = attributes 
+        self.data = self.read_data(self.file_path)
         
     def read_data(self, file_path):
         data = []
@@ -11,15 +12,18 @@ class Data:
             with open(file_path, 'r') as file:
                 for line in file:
                     row = []
+                    i = 0
                     for cell in line.strip().split(self.delimiter):
-                        type = self.get_type(cell)
-                        row.append((cell, type))
+                        data_type = self.get_type(cell)
+                        attribute = self.attributes[i]
+                        print(attribute)
+                        row.append((cell, data_type, attribute))
+                        i += 1  # Increment i to move to the next attribute
                     data.append(row)
         except FileNotFoundError:
-            print(f"File not found: {file_path}")
-        
+            print(f"File not found: {file_path}")        
         return data
-        
+    
     def get_type(self, value):
         try:
             int(value)
@@ -31,8 +35,60 @@ class Data:
             except ValueError:
                 return 'string'
     
-    def show_data(self):
+    def get_column(self, column):
+        column_data = []
+        for row in self.data:
+            column_data.append(row[column])
+        return column_data
+    
+    def get_row(self, row):
+        return self.data[row]
+    
+    def get_number_of_rows(self):
+        return len(self.data)
+    
+    def get_number_of_columns(self):
+        return len(self.data[0])-1
+    
+    def get_attribute(self, attributes):
+        data = []
+        for i in range(len(self.attributes)):
+            if self.attributes[i] in attributes:
+                data.append(self.get_column(i))
+        return data
+    
+    def get_class(self, class_name):
+        data = []
+        for row in self.data:
+            if row[0][0] == class_name:
+                data.append(row)
+        return data
+    
+    def show_database(self):
         for row in self.data:
             for cell in row:
                 print(cell, end=' ')
+            print()               
+
+    def show_column(self, column):
+        for row in self.data:
+            print(row[column], end=' ')
+            print()    
+    
+    def show_row(self, row):
+        for cell in self.data[row]:
+            print(cell, end=' ')
             print()
+    
+    def show_data(self, data):
+        for row in data:
+            for cell in row:
+                print(cell, end=' ')
+                print()
+            print()
+    
+    def show_class(self, data):
+        for row in data:
+            for cell in row:
+                print(cell, end=' ')
+            print()    
--- PL1/main.py
+++ PL1/main.py
@@ -1,8 +1,26 @@
 from class_data import Data
+from class_file import File
 
 ARCHIVO = './wine.data'
+ARCHIVO1 = 'new.txt'
+ARCHIVO2 = 'new2.txt'
 DELIMITADOR = ','
+ATRIBUTOS = ['Class', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color Intensity', 'Hue', 'ODS280/ODS315 of diluted wines', 'Proline']
+CLASE = '1'
 
-base_datos = Data(ARCHIVO, DELIMITADOR)
+base_datos = Data(ARCHIVO, DELIMITADOR, ATRIBUTOS)
+archivo_1 = File(ARCHIVO1, DELIMITADOR)
+archivo_2 = File(ARCHIVO2, DELIMITADOR)
 
-base_datos.show_data()
+#base_datos.show_database()
+#base_datos.show_column(0)
+#base_datos.show_row(1)
+#print(base_datos.get_number_of_rows(),'Elementos')
+#print(base_datos.get_number_of_columns(),'Atributos')
+Matriz = base_datos.get_attribute(['Alcohol','Malic acid','Magnesium'])
+#base_datos.show_data(Matriz)
+Matriz2 = base_datos.get_class(CLASE)
+#base_datos.show_class(Matriz2)
+
+archivo_1.write_data(Matriz)
+archivo_2.write_data(Matriz2)

62fb1c5ebfa78f3fd3e4773187633f24dec3bbcc
Samuel Barush <100962136+SamuelBarush@users.noreply.github.com>
2024-01-02 21:14:34 -0600

    Update

--- Segundo Parcial/PL3/class_erros.py
+++ Segundo Parcial/PL3/class_erros.py
@@ -3,141 +3,52 @@ import numpy as np
 from math import sqrt
 
 class ERROR:
-    def __init__(self,data) ->None:
+    def __init__(self, data,clases) -> None:
         self.data = data
-        self.clase1,self.clase2,self.clase3 = self.contar()
+        self.clases = self.contar(clases)
 
-    def contar(self):
-        count = 0
-        count1 = 0
-        count2 = 0
+    def contar(self,clases):
+        counts = [0] * clases  # Inicializar una lista para contar las instancias de cada clase
 
         for i in range(len(self.data)):
-            if self.data[i][0][0] == '1':
-                count += 1
-            elif self.data[i][0][0] == '2':
-                count1 += 1
-            elif self.data[i][0][0] == '3':
-                count2 += 1
-        return count, count1, count2
+            clase = int(self.data[i][0][0])
+            counts[clase - 1] += 1
+
+        return counts
+
+    def porcentaje(self):
+        total = sum(self.clases)
+        porcentajes = [(count / total) * 100 for count in self.clases]
+        return porcentajes
     
-    def porcentaje(self,class1,class2,class3):
-        total = class1 + class2 + class3
-        porcentaje1 = (class1/total)*100
-        porcentaje2 = (class2/total)*100
-        porcentaje3 = (class3/total)*100
-        return porcentaje1,porcentaje2,porcentaje3
-    
-    def vacios(self):
+    def vacios(self,clases):
         count = 0
-        datos_clase1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        datos_clase2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        datos_clase3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        num_atributos = len(self.data[0])  # Obtener el número de atributos
+        
+        # Crear listas dinámicas para almacenar los datos de cada clase
+        datos_clase = [[0] * num_atributos for _ in range(clases)]
+        
         for i in range(len(self.data)):
-            for j in range(len(self.data[i])):
+            for j in range(num_atributos):
                 if self.data[i][j][0] == '0':
                     count += 1
-                    print(f"El dato vacio es: {self.data[i][j]} de la fila {i} y columna {j}")
-            if (self.data[i][0][0] == '1' ):
-                if self.data[i][1][0] == '0':
-                    datos_clase1 [1] += 1
-                elif self.data[i][2][0] == '0':
-                    datos_clase1 [2] += 1
-                elif self.data[i][3][0] == '0':
-                    datos_clase1 [3] += 1
-                elif self.data[i][4][0] == '0':
-                    datos_clase1 [4] += 1
-                elif self.data[i][5][0] == '0':
-                    datos_clase1 [5] += 1
-                elif self.data[i][6][0] == '0':
-                    datos_clase1 [6] += 1
-                elif self.data[i][7][0] == '0':
-                    datos_clase1 [7] += 1
-                elif self.data[i][8][0] == '0':
-                    datos_clase1 [8] += 1
-                elif self.data[i][9][0] == '0':
-                    datos_clase1 [9] += 1
-                elif self.data[i][10][0] == '0':
-                    datos_clase1 [10] += 1
-                elif self.data[i][11][0] == '0':
-                    datos_clase1 [11] += 1
-                elif self.data[i][12][0] == '0':
-                    datos_clase1 [12] += 1
-                elif self.data[i][13][0] == '0':
-                    datos_clase1 [13] += 1
-            elif (self.data[i][0][0] == '2' ):
-                if self.data[i][1][0] == '0':
-                    datos_clase2 [1] += 1
-                elif self.data[i][2][0] == '0':
-                    datos_clase2 [2] += 1
-                elif self.data[i][3][0] == '0':
-                    datos_clase2 [3] += 1
-                elif self.data[i][4][0] == '0':
-                    datos_clase2 [4] += 1
-                elif self.data[i][5][0] == '0':
-                    datos_clase2 [5] += 1
-                elif self.data[i][6][0] == '0':
-                    datos_clase2 [6] += 1
-                elif self.data[i][7][0] == '0':
-                    datos_clase2 [7] += 1
-                elif self.data[i][8][0] == '0':
-                    datos_clase2 [8] += 1
-                elif self.data[i][9][0] == '0':
-                    datos_clase2 [9] += 1
-                elif self.data[i][10][0] == '0':
-                    datos_clase2 [10] += 1
-                elif self.data[i][11][0] == '0':
-                    datos_clase2 [11] += 1
-                elif self.data[i][12][0] == '0':
-                    datos_clase2 [12] += 1
-                elif self.data[i][13][0] == '0':
-                    datos_clase2 [13] += 1
-            elif (self.data[i][0][0] == '3' ):
-                if self.data[i][1][0] == '0':
-                    datos_clase3 [1] += 1
-                elif self.data[i][2][0] == '0':
-                    datos_clase3 [2] += 1
-                elif self.data[i][3][0] == '0':
-                    datos_clase3 [3] += 1
-                elif self.data[i][4][0] == '0':
-                    datos_clase3 [4] += 1
-                elif self.data[i][5][0] == '0':
-                    datos_clase3 [5] += 1
-                elif self.data[i][6][0] == '0':
-                    datos_clase3 [6] += 1
-                elif self.data[i][7][0] == '0':
-                    datos_clase3 [7] += 1
-                elif self.data[i][8][0] == '0':
-                    datos_clase3 [8] += 1
-                elif self.data[i][9][0] == '0':
-                    datos_clase3 [9] += 1
-                elif self.data[i][10][0] == '0':
-                    datos_clase3 [10] += 1
-                elif self.data[i][11][0] == '0':
-                    datos_clase3 [11] += 1
-                elif self.data[i][12][0] == '0':
-                    datos_clase3 [12] += 1
-                elif self.data[i][13][0] == '0':
-                    datos_clase3 [13] += 1
-        
-        for i in range (1,len(datos_clase1)):
-            porcentaje1 = (datos_clase1[i]/self.clase1)*100
-            print(f"El porcentaje de datos vacios de la clase 1 en el atributo {i} es: {porcentaje1}")
-        for i in range (1,len(datos_clase2)):
-            porcentaje2 = (datos_clase2[i]/self.clase2)*100
-            print(f"El porcentaje de datos vacios de la clase 2 en el atributo {i} es: {porcentaje2}")
-        for i in range (1,len(datos_clase3)):
-            porcentaje3 = (datos_clase3[i]/self.clase3)*100
-            print(f"El porcentaje de datos vacios de la clase 3 en el atributo {i} es: {porcentaje3}")
-        return count, datos_clase1, datos_clase2, datos_clase3
+                    print(f"El dato vacío es: {self.data[i][j]} de la fila {i} y columna {j}")
+            
+            clase = int(self.data[i][0][0])
+            for k in range(1, num_atributos):
+                if self.data[i][k][0] == '0':
+                    datos_clase[clase - 1][k] += 1
+
+        # Imprimir resultados de datos vacíos por clase
+        for clase, datos in enumerate(datos_clase, 1):
+            print(f"Datos vacíos en la clase {clase}: {datos}")
+
+        return count
     
     def min(self):
         minimo = []
-        for i in range (1,len(self.data[0])):
-            #if (self.data[0][i][0] != '0'):
-            minimo.append(self.data[0][i])
-            #else:
-            #    minimo.append(self.data[1][i])
+        for i in range (1,len(self.data[0])):   #No poner 0 en la fila 0
+            minimo.append(self.data[0][i])      #Hay un bug
             for j in range (len(self.data)):
                 if (self.data[j][i] < minimo[i-1]):
                     if (self.data[j][i][0] != '0'):
@@ -217,31 +128,3 @@ class ERROR:
         for i in range (len(self.data)):
             self.data[i].pop(columna)
         return self.data
-    # ------------------------ 2 ------------------------
-    
-    def vacios_2(self):
-        count = 0
-        datos_clase1 = [0] * len(self.data[0])
-        datos_clase2 = [0] * len(self.data[0])
-        datos_clase3 = [0] * len(self.data[0])
-
-        for i in range(len(self.data)):
-            for j in range(1, len(self.data[i])):  # Comenzamos desde 1 para omitir la clase
-                if self.data[i][j][0] == '0':
-                    count += 1
-                    if self.data[i][0][0] == '1':
-                        datos_clase1[j] += 1
-                    elif self.data[i][0][0] == '2':
-                        datos_clase2[j] += 1
-                    elif self.data[i][0][0] == '3':
-                        datos_clase3[j] += 1
-
-        for i in range(1, len(self.data[0])):
-            print(f"Atributo {i}:")
-            print(f"  De  faltan {datos_clase1[i]} ({(datos_clase1[i] / count) * 100}%)")
-            print(f"  De  faltan {datos_clase2[i]} ({(datos_clase2[i] / count) * 100}%)")
-            print(f"  De  faltan {datos_clase3[i]} ({(datos_clase3[i] / count) * 100}%)")
-
-        return count, datos_clase1, datos_clase2, datos_clase3
-    
-    
--- Segundo Parcial/PL3/main.py
+++ Segundo Parcial/PL3/main.py
@@ -9,7 +9,6 @@ ARCHIVO1 = 'new.txt'
 ARCHIVO2 = 'new2.txt'
 ARCHIVO3 = 'new3.txt'
 DELIMITADOR = ','
-#ATRIBUTOS = ['Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color Intensity', 'Hue', 'ODS280/ODS315 of diluted wines', 'Proline']
 ATRIBUTOS = ['Class', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color Intensity', 'Hue', 'ODS280/ODS315 of diluted wines', 'Proline']
 
 SUBCONJUNTO = 50
@@ -38,31 +37,15 @@ Matriz01 = base_datos.get_rows_range(Matriz1,1,10)
 
 #Detectar el porcentaje de muestras
 
-error = ERROR(Matriz01)
+error = ERROR(base_datos.data,3)
 
 #print_matrix(error.normalizar())
 
 #print(error.desviacion_estandar(1))
 #print(error.media(1))
 
-print_matrix(error.data)
-error.eliminar_columna(1)
-print_matrix(error.data)
+#print_matrix(error.data)
+#error.eliminar_columna(1)
+#print_matrix(error.data)
 
-
-#class1,class2,class3 = error.contar()
-#print(f"El numero de muestras de la clase 1 es: {class1}")
-#print(f"El numero de muestras de la clase 2 es: {class2}")  
-#print(f"El numero de muestras de la clase 3 es: {class3}")
-
-#porcentaje1,porcentaje2,porcentaje3 = error.porcentaje(class1,class2,class3)
-#print(f"El porcentaje de muestras de la clase 1 es: {porcentaje1}") 
-#print(f"El porcentaje de muestras de la clase 2 es: {porcentaje2}") 
-#print(f"El porcentaje de muestras de la clase 3 es: {porcentaje3}") 
-
-#vacios = error.vacios()
-#print(f"El numero de muestras vacias es: {vacios}")
-
-#print(f"El numero de muestras vacias de la clase 1 es: {vacios[1]}")    
-#print(f"El numero de muestras vacias de la clase 2 es: {vacios[2]}")
-#print(f"El numero de muestras vacias de la clase 3 es: {vacios[3]}")
+error.vacios(3)
